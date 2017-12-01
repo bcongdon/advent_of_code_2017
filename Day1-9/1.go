8e48f976781698344e242c40bcff0e644025faf0
Benjamin Congdon <bencongdon96@gmail.com>
2017-12-01 11:24:29 -0600

    Day1 in Golang

---
+++ Day1-9/1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "io/ioutil"
+    "strconv"
+    "fmt"
+    "strings"
+)
+
+func captcha(input string) (part1 int, part2 int) {
+    part1 = 0
+    part2 = 0
+    n := len(input)
+    for i := 0; i < n; i++ {
+        cval, _ := strconv.Atoi(string(input[i]))
+        if input[(i+1)%n] == input[i] {
+            part1 += cval
+        }
+        if input[(i+n/2)%n] == input[i] {
+            part2 += cval
+        }
+    }
+    return
+}
+
+func main() {
+    dat, _ := ioutil.ReadFile("./1.txt")
+    part1, part2 := captcha(strings.TrimSpace(string(dat)))
+    fmt.Printf("Part 1: %d\n", part1)
+    fmt.Printf("Part 2: %d\n", part2)
+}
