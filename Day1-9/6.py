14155e8b192040869e1e562d6bd4579b00d83b58
Benjamin Congdon <bencongdon96@gmail.com>
2017-12-06 10:32:32 -0600

    Perf improvements to Day6 Python; Day6 in Golang

--- Day1-9/6.py
+++ Day1-9/6.py
@@ -1,21 +1,21 @@
-def serialize(bins):
-    return ','.join(map(str, bins))
-
-
 def cycles_until_reseen(bins):
     configs = set()
 
     curr = bins
-    while serialize(curr) not in configs:
-        configs.add(serialize(curr))
+    while tuple(curr) not in configs:
+        configs.add(tuple(curr))
         min_idx = 0
         for i in range(1, len(curr)):
             if curr[i] > curr[min_idx]:
                 min_idx = i
         redistribute = curr[min_idx]
         curr[min_idx] = 0
-        for i in range(redistribute):
-            curr[(min_idx+i+1) % len(curr)] += 1
+        all_gains = redistribute // len(bins)
+        partial_gain_bins = redistribute % len(bins)
+        for i in range(len(bins)):
+            curr[i] += all_gains
+            if i < partial_gain_bins:
+                curr[(min_idx+i+1) % len(bins)] += 1
     return len(configs), curr
 
 if __name__ == '__main__':
