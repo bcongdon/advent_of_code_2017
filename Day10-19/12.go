2112371b1d13fee39fbebe5e594de9a80a7f3d20
Benjamin Congdon <bencongdon96@gmail.com>
2017-12-12 07:34:31 -0600

    Go fmt

--- Day10-19/12.go
+++ Day10-19/12.go
@@ -1,69 +1,79 @@
 package main
 
 import (
-    "io/ioutil"
-    "fmt"
-    "github.com/golang-collections/collections/stack"
-    "strings"
+	"fmt"
+	"github.com/golang-collections/collections/stack"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
-func graphReach(graph map[string][]string, start string) []string{
-    toVisit := stack.New()
-    toVisit.Push(start)
-    reachable := make(map[string]bool)
-    for ; toVisit.Len() > 0; {
-        curr := toVisit.Pop().(string)
-        for _, neighbor := range graph[curr] {
-            if _, ok := reachable[neighbor]; !ok {
-                reachable[neighbor] = true
-                toVisit.Push(neighbor)
-            }
-        }
-    }
+func graphReach(graph map[int][]int, start int) []int {
+	toVisit := stack.New()
+	toVisit.Push(start)
+	
+	reachable := make(map[int]bool)
+	for toVisit.Len() > 0 {
+		curr := toVisit.Pop().(int)
+		for _, neighbor := range graph[curr] {
+			if _, ok := reachable[neighbor]; !ok {
+				reachable[neighbor] = true
+				toVisit.Push(neighbor)
+			}
+		}
+	}
 
-    keys := []string{}
-    for k := range reachable {
-        keys = append(keys, k)
-    }
-    return keys
+	keys := []int{}
+	for k := range reachable {
+		keys = append(keys, k)
+	}
+	
+	return keys
 }
 
-func numConnectedComponents(graph map[string][]string) int {
-    marked := make(map[string]bool)
-    numComponents := 0
-    for vertex := range graph {
-        if _, ok := marked[vertex]; ok {
-            continue
-        }
+func numConnectedComponents(graph map[int][]int) int {
+	marked := make(map[int]bool)
+	numComponents := 0
+	for vertex := range graph {
+		if _, ok := marked[vertex]; ok {
+			continue
+		}
 
-        component := graphReach(graph, vertex)
-        for _, c := range component {
-            marked[c] = true
-        }
-        numComponents++
-    }
-    return numComponents
+		component := graphReach(graph, vertex)
+		for _, c := range component {
+			marked[c] = true
+		}
+		numComponents++
+	}
+	return numComponents
 }
 
-func parseGraph(edges []string) map[string][]string {
-    graph := make(map[string][]string)
-    replacer := strings.NewReplacer(",", "", "\n", "")
-    for _, e := range edges {
-        split := strings.Split(e, " ")
-        adjacent := make([]string, len(split) - 2)
-        for i, v := range split[2:] {
-            adjacent[i] = replacer.Replace(v)
-        }
-        graph[split[0]] = adjacent
-    }
-    return graph
+func parseGraph(edges []string) map[int][]int {
+	graph := make(map[int][]int)
+	replacer := strings.NewReplacer(",", "", "\n", "")
+	for _, e := range edges {
+		split := strings.Split(e, " ")
+		adjacent := make([]int, len(split)-2)
+		
+		for i, v := range split[2:] {
+			cleanVStr := replacer.Replace(v)
+			adjVal, _ := strconv.Atoi(cleanVStr)
+			adjacent[i] = adjVal
+		}
+
+		vertexVal, _ := strconv.Atoi(split[0])
+		graph[vertexVal] = adjacent
+	}
+	return graph
 }
 
 func main() {
-    dat, _ := ioutil.ReadFile("./12.txt")
-    graph := parseGraph(strings.Split(string(dat), "\n"))
-    part1 := graphReach(graph, "0")
-    fmt.Printf("Part 1: %d\n", len(part1))
-    part2 := numConnectedComponents(graph)
-    fmt.Printf("Part 2: %d\n", part2)
+	dat, _ := ioutil.ReadFile("./12.txt")
+	graph := parseGraph(strings.Split(string(dat), "\n"))
+	
+	part1 := graphReach(graph, 0)
+	fmt.Printf("Part 1: %d\n", len(part1))
+	
+	part2 := numConnectedComponents(graph)
+	fmt.Printf("Part 2: %d\n", part2)
 }
