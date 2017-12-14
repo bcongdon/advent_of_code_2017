084a2acce9ab2c57c87fa044f62c80ef709a5ef7
Benjamin Congdon <bencongdon96@gmail.com>
2017-12-14 07:52:58 -0600

    Day14 in Python

--- Day10-19/10.py
+++ Day10-19/10.py
@@ -3,7 +3,7 @@ import numpy as np
 magic_suffix = [17, 31, 73, 47, 23]
 
 
-def knot_hash(inputs, size=256, curr=0, skip=0, knot=None):
+def knot_hash_iter(inputs, size=256, curr=0, skip=0, knot=None):
     if knot is None:
         knot = np.arange(0, size)
     for i in inputs:
@@ -23,16 +23,16 @@ def knot_hash(inputs, size=256, curr=0, skip=0, knot=None):
 
 
 def part1(inputs, size=256):
-    lengths, _, _ = knot_hash(inputs, size=size)
+    lengths, _, _ = knot_hash_iter(inputs, size=size)
     return lengths[0] * lengths[1]
 
 
-def part2(input_str):
+def knot_hash(input_str):
     lengths = [ord(i) for i in input_str] + magic_suffix
     sparse_hash = None
     curr, skip = 0, 0
     for _ in range(64):
-        sparse_hash, curr, skip = knot_hash(
+        sparse_hash, curr, skip = knot_hash_iter(
             lengths, curr=curr, skip=skip, knot=sparse_hash
         )
     dense_hash = [0 for _ in range(16)]
@@ -42,8 +42,8 @@ def part2(input_str):
 
 if __name__ == '__main__':
     # assert part1([3, 4, 1, 5], size=5) == 12
-    # assert part2('') == 'a2582a3a0e66e6e86e3812dcb672a272'
-    # assert part2('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+    # assert knot_hash('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    # assert knot_hash('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
 
     with open("10.txt") as f:
         raw = f.read().strip()
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     product = part1(lengths)
     print("Part 1: {}".format(product))
 
-    dense_hash = part2(raw)
+    dense_hash = knot_hash(raw)
     print("Part 2: {}".format(dense_hash))
---
+++ Day10-19/14.py
@@ -0,0 +1,88 @@
+import numpy as np
+import binascii
+magic_suffix = [17, 31, 73, 47, 23]
+
+
+def knot_hash_iter(inputs, size=256, curr=0, skip=0, knot=None):
+    if knot is None:
+        knot = np.arange(0, size)
+    for i in inputs:
+        if curr + i < size:
+            chunk = knot[curr:curr+i]
+        else:
+            chunk = np.append(knot[curr:], knot[:i-(size-curr)])
+        chunk = chunk[::-1]
+        if curr + i < size:
+            knot[curr:curr+i] = chunk
+        else:
+            knot[curr:] = chunk[:size-curr]
+            knot[:i-(size-curr)] = chunk[size-curr:]
+        curr = (curr + (i + skip)) % size
+        skip += 1
+    return knot, curr, skip
+
+
+def knot_hash(input_str):
+    lengths = [ord(i) for i in input_str] + magic_suffix
+    sparse_hash = None
+    curr, skip = 0, 0
+    for _ in range(64):
+        sparse_hash, curr, skip = knot_hash_iter(
+            lengths, curr=curr, skip=skip, knot=sparse_hash
+        )
+    dense_hash = [0 for _ in range(16)]
+    for i in range(16):
+        dense_hash[i] = np.bitwise_xor.reduce(sparse_hash[16*i:16*i+16])
+    return(bytes(dense_hash).hex())
+
+
+def construct_grid(seed):
+    for i in range(128):
+        key = "{}-{}".format(seed, i)
+        row_hash = knot_hash(key)
+        binary = binascii.unhexlify(row_hash)
+        row = ''
+        for b in binary:
+            b_str = bin(b)
+            b_str = (10 - len(b_str))*'0' + b_str[2:]
+            row += b_str
+        yield row
+
+
+def part1(grid):
+    return sum(row.count('1') for row in grid)
+
+
+def part2(grid):
+    visited = set()
+
+    def dfs(x, y):
+        if grid[x][y] != '1' or (x, y) in visited:
+            return
+        visited.add((x, y))
+        if x < len(grid)-1:
+            dfs(x+1, y)
+        if y < len(grid[0])-1:
+            dfs(x, y+1)
+        if x > 0:
+            dfs(x-1, y)
+        if y > 0:
+            dfs(x, y-1)
+
+    group_count = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if (x, y) not in visited and grid[x][y] == '1':
+                dfs(x, y)
+                group_count += 1
+    return group_count
+
+
+if __name__ == '__main__':
+    puzzle_input = 'uugsqrei'
+    grid = [row for row in construct_grid(puzzle_input)]
+    p1 = part1(grid)
+    print("Part 1: {}".format(p1))
+
+    p2 = part2(grid)
+    print("Part 2: {}".format(p2))
